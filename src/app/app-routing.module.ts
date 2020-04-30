feed3a4b89c2e90f3a9104ca8fb0f3c6cec09aa3
DavidSunny92 <davidm060592@gmail.com>
2020-04-30 17:25:57 +0530

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,18 @@ import { SubmitComponent } from "./components/submit/submit.component";
 import { HooksComponent } from "./components/hooks/hooks.component";
 import { DatabindingComponent } from "./components/databinding/databinding.component";
 import { FormsComponent } from "./components/forms/forms.component";
+import { ViewEncapsulationComponent } from "./components/view-encapsulation/view-encapsulation.component";
 
 const routes: Routes = [
   { path: "data-share", component: SubmitComponent },
   { path: "lifcycle-hooks", component: HooksComponent },
   { path: "data-binding", component: DatabindingComponent },
   { path: "forms", component: FormsComponent },
+  { path: "view-encaps", component: ViewEncapsulationComponent },
+  {
+    path: "client",
+    loadChildren: "./components/client/client/client.module#ClientModule",
+  },
 ];
 
 @NgModule({
